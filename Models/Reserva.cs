0a958ef099b2c01e470dff5b558c5b4899244c3f
Guilherme-Gomes3 <guilhermeGomes@fam.edu.br>
2026-02-22 23:50:19 -0300

    .

---
+++ Models/Reserva.cs
@@ -0,0 +1,52 @@
+namespace DesafioProjetoHospedagem.Models
+{
+    public class Reserva
+    {
+        public List<Pessoa> Hospedes { get; set; }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public Reserva() { }
+
+        public Reserva(int diasReservados)
+        {
+            DiasReservados = diasReservados;
+        }
+
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+            // Verifica se a capacidade da suíte é suficiente
+            if (Suite.Capacidade >= hospedes.Count)
+            {
+                Hospedes = hospedes;
+            }
+            else
+            {
+                throw new Exception("A quantidade de hóspedes excede a capacidade da suíte.");
+            }
+        }
+
+        public void CadastrarSuite(Suite suite)
+        {
+            Suite = suite;
+        }
+
+        public int ObterQuantidadeHospedes()
+        {
+            return Hospedes.Count;
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+            decimal valor = DiasReservados * Suite.ValorDiaria;
+
+            // Desconto de 10% para reservas >= 10 dias
+            if (DiasReservados >= 10)
+            {
+                valor -= valor * 0.10m;
+            }
+
+            return valor;
+        }
+    }
+}
